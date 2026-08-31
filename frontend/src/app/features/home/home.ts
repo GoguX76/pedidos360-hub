8e8f4e4aba15260d63783b2f8000a5203ad6cdd4
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-08-31 09:28:24 -0400

    feat: implementar página home

--- frontend/src/app/features/home/home.ts
+++ frontend/src/app/features/home/home.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../core/auth/auth.service';
+import { NavCardComponent } from '../../shared/ui/nav-card/nav-card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [NavCardComponent],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+  protected authService = inject(AuthService);
+
+  protected onLogoutClick(): void {
+    this.authService.logOut();
+  }
+}
